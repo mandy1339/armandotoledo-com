e8edc0f61946e4fecc5ce15265d1d2c0c216205d
mandy1339 <mandy1339@gmail.com>
2017-12-12 02:07:33 -0500

    automated script commit

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
 			<li ><a href="https://animal-game.herokuapp.com/">Guess Your Animal</a></li>
 			<li><a href="http://www.cryptoalert.armandotoledo.com/"</a>Crypto Alert</li>
 			<li><a href="http://blog.armandotoledo.com/"</a>My Blog</li>
-			<li><a href="http://www.armadoro.com/"</a>Armadoro (agile project management) <em>collaborated with 1 Adora Smith</em></li>
+			<li><a href="http://www.armadoro.com/"</a>Armadoro (agile project management) - collaborated with 1 Adora Smith</li>
 			<li><a href="http://www.armandotoledo.com/"</a>My Home Site</li>
 		</ul>
 	</div>
