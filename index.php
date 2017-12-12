5bb2c49ebe0ddbd067ad1b1509efe44456fb61e6
mandy1339 <mandy1339@gmail.com>
2017-12-12 02:09:29 -0500

    automated script commit

--- index.php
+++ index.php
@@ -13,10 +13,10 @@
 	<div class="ControlSection well">
 		<h4>My Web Applications</h4>
 		<ul >
-			<li ><a href="https://animal-game.herokuapp.com/">Guess Your Animal</a></li>
-			<li><a href="http://www.cryptoalert.armandotoledo.com/"</a>Crypto Alert</li>
-			<li><a href="http://blog.armandotoledo.com/"</a>My Blog</li>
-			<li><a href="http://www.armadoro.com/"</a>Armadoro (agile project management) - collaborated with 1 Adora Smith</li>
+			<li><a href="http://www.cryptoalert.armandotoledo.com/"</a><strong>Crypto Alert</strong></li>
+			<li><a href="http://blog.armandotoledo.com/"</a><strong>My Blog</strong></li>
+			<li><a href="http://www.armadoro.com/"</a><strong>Armadoro</strong> (agile project management) - collaborated with 1 Adora Smith</li>
+			<li ><a href="https://animal-game.herokuapp.com/"><strong>Guess Your Animal</strong></a></li>
 			<li><a href="http://www.armandotoledo.com/"</a>My Home Site</li>
 		</ul>
 	</div>
