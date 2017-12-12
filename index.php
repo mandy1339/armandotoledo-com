e1ae989d9da38781a19b6f0f49d50af01e39bf90
mandy1339 <mandy1339@gmail.com>
2017-12-12 01:52:51 -0500

    added fonts, background images, and css

--- index.php
+++ index.php
@@ -2,22 +2,27 @@
 
 <?php require('header.php'); ?>
 
+<div id="header1" class="center well ">
+	<h1 class="well">Armando Toledo's Site</h1>
+</div>
 
-<h1>Armando Toledo's Site</h1>
+<br>
+<br>
+
 <main>
-	<div>
+	<div class="ControlSection well">
 		<h4>My Web Applications</h4>
-		<ul class="list-group">
-			<li class="list-group-item"><a href="https://animal-game.herokuapp.com/">Guess Your Animal</a></li>
-			<li class="list-group-item"><a href="http://www.cryptoalert.armandotoledo.com/"</a>Crypto Alert</li>
-			<li class="list-group-item"><a href="http://blog.armandotoledo.com/"</a>My Blog</li>
-			<li class="list-group-item"><a href="http://www.armadoro.com/"</a>Armadoro (agile project management) <em>collaborated with 1 Adora Smith</em></li>
-			<li class="list-group-item"><a href="http://www.armandotoledo.com/"</a>My Home Site</li>
+		<ul >
+			<li ><a href="https://animal-game.herokuapp.com/">Guess Your Animal</a></li>
+			<li><a href="http://www.cryptoalert.armandotoledo.com/"</a>Crypto Alert</li>
+			<li><a href="http://blog.armandotoledo.com/"</a>My Blog</li>
+			<li><a href="http://www.armadoro.com/"</a>Armadoro (agile project management) <em>collaborated with 1 Adora Smith</em></li>
+			<li><a href="http://www.armandotoledo.com/"</a>My Home Site</li>
 		</ul>
 	</div>
 </main>
 
-<?php r//equire("footer.php"); ?>
+<?php //require("footer.php"); ?>
 
 </html>
 
