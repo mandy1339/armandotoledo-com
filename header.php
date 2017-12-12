f6a7e526d1cd343094e4bcfeb653f918e8d14a4f
Ubuntu <ubuntu@ip-172-31-46-210.ec2.internal>
2017-12-12 05:52:00 +0000

    second commit?

---
+++ header.php
@@ -0,0 +1,9 @@
+<head>
+	<title>Armando's Site</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+	<link rel="stylesheet" href="style1.css">
+
+</head>
+
+
+<body>
---
+++ index.php
@@ -0,0 +1,23 @@
+
+
+<?php require('header.php'); ?>
+
+
+<h1>Armando Toledo's Site</h1>
+<main>
+	<div>
+		<h4>My Web Applications</h4>
+		<ul class="list-group">
+			<li class="list-group-item"><a href="https://animal-game.herokuapp.com/">Guess Your Animal</a></li>
+			<li class="list-group-item"><a href="http://www.cryptoalert.armandotoledo.com/"</a>Crypto Alert</li>
+			<li class="list-group-item"><a href="http://blog.armandotoledo.com/"</a>My Blog</li>
+			<li class="list-group-item"><a href="http://www.armadoro.com/"</a>Armadoro (agile project management) <em>collaborated with 1 Adora Smith</em></li>
+			<li class="list-group-item"><a href="http://www.armandotoledo.com/"</a>My Home Site</li>
+		</ul>
+	</div>
+</main>
+
+<?php r//equire("footer.php"); ?>
+
+</html>
+
